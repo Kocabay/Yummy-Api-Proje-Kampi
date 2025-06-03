64077bcc8a986c9e876018fc0258fc5ecd42b201
Kocabay <Kocabay1965.>
2025-06-03 13:14:49 +0300

    UI

---
+++ ApiProjeKampi.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,100 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Dtos.ProductDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IValidator<Product> _validator;
+        private readonly ApiContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IValidator<Product> validator, ApiContext context, IMapper mapper)
+        {
+            _validator = validator;
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var values = _context.Products.ToList();
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            var valadationResult = _validator.Validate(product);  //product nesnesini doğrulamak için validator kullanılır
+            if (!valadationResult.IsValid) //eğer doğrulama başarısız ise
+            {
+                return BadRequest(valadationResult.Errors.Select(x => x.ErrorMessage)); //hataları döndür
+            }
+            else
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return Ok("Ürün Ekleme İşlemi Başarılı... ");
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = _context.Products.Find(id);
+            _context.Products.Remove(value);
+            _context.SaveChanges();
+            return Ok("Ürün Silme İşlemi Başarılı ...");
+        }
+
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _context.Products.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            var valadationResult = _validator.Validate(product);  //product nesnesini doğrulamak için validator kullanılır
+            if (!valadationResult.IsValid) //eğer doğrulama başarısız ise
+            {
+                return BadRequest(valadationResult.Errors.Select(x => x.ErrorMessage));
+            }
+            else
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                return Ok("Ürün Güncelle İşlemi Başarılı... ");
+            }
+        }
+        [HttpPost("CreateProductWithCategory")]
+        public IActionResult CreateProductWithCategory(CreateProductDto createProductDto)
+        {
+            var value = _mapper.Map<Product>(createProductDto);
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return Ok("Ürün Kategori ile Ekleme İşlemi Başarılı... ");
+        }
+
+        [HttpGet("ProductListWithCategory")]
+        public IActionResult ProductListWithCategory()
+        {
+            var value = _context.Products.Include(x => x.Category).ToList(); //Category ile birlikte listeleme
+            return Ok(_mapper.Map<List<ResultProductWithCategoryDto>>(value));
+        }
+
+    }
+}
---
+++ ApiProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,19 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjeKampi.WebApi.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün Adı Boş Geçilemez ...");
+            RuleFor(x => x.ProductName).MinimumLength(3).WithMessage("Ürün Adı En Az 3 Karakter Olmalıdır ...");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("Ürün Adı En Fazla 50 Karakter Olmalıdır ...");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün Fiyatı Boş Geçilemez ...").GreaterThan(0).WithMessage("Ürün Fiyatı Negatif Olamaz").LessThan(1000).WithMessage("Ürün Fiyatı bu kadar yüksek olamaz, girdiğiniz fiyatı kontrol ediniz..");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün Açıklaması Boş Geçilemez ...");
+        }
+    }
+}
